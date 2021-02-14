0116644b913793304121af0f1118a6d6d3913215
Gergő Kiss <gregkiss98@users.noreply.github.com>
2021-02-14 21:50:05 +0100

    rest of the main, new unit code

---
+++ Unit.java
@@ -0,0 +1,43 @@
+public class Unit{
+   
+    int atk;
+    int hp;
+
+    public Unit(int atk, int hp)
+    {
+        this.atk=atk;
+        this.hp=hp;
+    }
+
+    public boolean isAlive(Unit focus) //focus = akit épp vizsgálunk hogy él-e
+    {
+        if(focus.hp>0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public void attack(Unit defender){
+        int defenderHp = defender.getHealth();
+        int myAttack = getAtk();
+        int newHp = defenderHp - myAttack;
+        
+        defender.setHealth(newHp);
+    }
+
+    public int getHealth(){
+        return hp;
+    }
+    
+    public int getAtk(){
+        return atk;
+    }
+
+    private void setHealth(int hp) {
+        this.hp = hp;
+    }
+}
--- main.java
+++ main.java
@@ -1,5 +1,27 @@
 public class Main{
-    private static void Main(String args[]) {
-        
+    
+    public static void main(String[] args) {
+        Unit Knight1 = new Unit(5,10);
+        Unit Knight2 = new Unit(6,10);
+
+        battle(Knight1, Knight2);
+    }
+
+    public static void battle(Unit Knight1, Unit Knight2) {
+        int i = 1;
+        while (Knight1.isAlive(Knight1) && Knight2.isAlive(Knight2)) {
+            System.out.println(i + ". kör");
+            
+            System.out.println(Knight1.getHealth() + "hp-ja van az első lovagnak, " + Knight1.getAtk() + " sebzést tud okozni");
+            System.out.println(Knight2.getHealth() + "hp-ja van az első lovagnak" + Knight2.getAtk() + " sebzést tud okozni");
+            
+            Knight1.attack(Knight2);
+            Knight2.attack(Knight1);
+
+
+            i++;
+        }
+        String winner = Knight1.isAlive(Knight1) ? "Knight1 nyert" : "Knight2 nyert";
+        System.out.println(winner);
     }
 }
