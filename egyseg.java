797ffbb9ab92d774891c2abeeab04664ad79e667
unknown <kiss.viktor250@gmail.com>
2021-02-14 20:23:35 +0100

    Finished unit class

--- egyseg.java
+++ egyseg.java
@@ -1,10 +1,22 @@
-public class egyseg{
+public class unit{
     int atk;
     int def;
-    public egyseg(int atk, int def)
+    public unit(int atk, int def)
     {
         atk=atk;
         def=def;
     }
-    
+    public boolean isAlive(unit focus)
+    {
+        if(focus.def>0)
+        {
+            return false;
+        }
+        else{return true;}
+    }
+    public attack(unti attacker, unit deffender){
+            if(isAlive(deffender)==true){
+            deffender.def-=attacker.atk;}
+            else{system.out.println("It's already dead")}
+    }
 }
