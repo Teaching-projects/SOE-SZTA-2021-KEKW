3c2f17d0610ba15d1ab6fecd4db12db8dfdcfd68
unknown <kiss.viktor250@gmail.com>
2021-02-19 15:29:27 +0100

    Added user input

--- main.java
+++ main.java
@@ -1,9 +1,18 @@
 class Main{
     
     public static void main(String[] args) {
+        System.out.println("Give attack for Knigh1");
+        int Knight1ATK=ReadLine();
+        System.out.println("Now hp");
+        int Knight1HP=ReadLine();
 
-        Unit Knight1 = new Unit(5,10);
-        Unit Knight2 = new Unit(6,10);
+        System.out.println("Give attack for Knigh2");
+        int Knight2ATK=ReadLine();
+        System.out.println("Now hp");
+        int Knight2HP=ReadLine();
+
+        Unit Knight1 = new Unit(Knight1ATK,Knight1HP);
+        Unit Knight2 = new Unit(Knight2ATK,Knight2HP);
 
         battle(Knight1, Knight2);
     }
@@ -25,5 +34,7 @@ class Main{
         String winner = Knight1.isAlive() ? "Knight1 nyert" : "Knight2 nyert";
         System.out.println(winner);        
     }
-
+    public static int ReadLine (){
+        return Integer.parseInt(System.console().readLine());
+    }
 }
