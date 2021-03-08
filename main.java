0b5da7b91411e4f3369a3320121aa47da2c1ba5e
Gergő Kiss <gregkiss98@users.noreply.github.com>
2021-03-08 13:16:09 +0100

    small update
    első körben mindkét lovag megüti egymást

--- main.java
+++ main.java
@@ -24,20 +24,31 @@ class Main{
 
     public static void battle(Unit Knight1, Unit Knight2) {
         int i = 1;
+
         while (Knight1.isAlive() && Knight2.isAlive()) {
-            System.out.println(i + ". kör");
-            
-            if(Knight1.isAlive() && Knight1.canHit(i))
+            if (i == 1) //az első körben mindkét lovag megüti egymást
             {
-            System.out.println(Knight1.getHealth() + "hp-ja van az első lovagnak, " + Knight1.getAtk() + " sebzést tud okozni");
-            Knight1.attack(Knight2);
-            }
-            else if(Knight2.isAlive() && Knight2.canHit(i)){
-            System.out.println(Knight2.getHealth() + "hp-ja van az második lovagnak, " + Knight2.getAtk() + " sebzést tud okozni");
-            Knight2.attack(Knight1);
+                System.out.println(i + ". kör");
+                System.out.println(Knight1.getHealth() + "hp-ja van az első lovagnak, " + Knight1.getAtk() + " sebzést tud okozni");
+                Knight1.attack(Knight2);
+                System.out.println(Knight2.getHealth() + "hp-ja van az második lovagnak, " + Knight2.getAtk() + " sebzést tud okozni");
+                Knight2.attack(Knight1);
             }
             else{
+                System.out.println(i + ". kör");
+
+                if(Knight1.isAlive() && Knight1.canHit(i))
+                {
+                System.out.println(Knight1.getHealth() + "hp-ja van az első lovagnak, " + Knight1.getAtk() + " sebzést tud okozni");
+                Knight1.attack(Knight2);
+                }
+                else if(Knight2.isAlive() && Knight2.canHit(i)){
+                System.out.println(Knight2.getHealth() + "hp-ja van az második lovagnak, " + Knight2.getAtk() + " sebzést tud okozni");
+                Knight2.attack(Knight1);
+                }
+                else{
                 System.out.println("Egyik lovag sem támadott ebben a körben");
+                }
             }
 
             i++;
